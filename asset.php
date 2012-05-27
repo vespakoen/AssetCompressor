4906a1713c64197846ed1fbe7d5f18357b6e8a7e
Koen Schmeets <k.schmeets@gmail.com>
2012-05-27 13:29:04 +0200

    Fixing config path

--- asset.php
+++ asset.php
@@ -13,8 +13,8 @@ class Asset_Container extends Laravel_Asset_Container {
 	public function __construct($name)
 	{
 		parent::__construct($name);
-		
-		$this->config = Config::get('layla_thirdparty_assetcompressor::assetcompressor');
+
+		$this->config = Config::get('assetcompressor::assetcompressor');
 		$this->config['cache_dir'] = $this->config['cache_dir'] . '/';
 		$this->config['cache_dir_path'] = path('public').$this->config['cache_dir'];
 
